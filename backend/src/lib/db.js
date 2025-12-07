605479214592d966823ff77f34b6d097bd069a7a
Shubham Kshirsagar <kshirsagarshubham28101999@gmail.com>
2025-12-07 11:07:55 +0530

    backend deploy

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
-    process.exit(1); // exit process on DB connection failure
+    process.exit(1);
   }
 };
